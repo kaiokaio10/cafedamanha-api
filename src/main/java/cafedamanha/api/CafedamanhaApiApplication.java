ee9195b6454e4f580401b3b201ccc6b1bd574b1a
kaiokaio10 <kgalvo47@gmail.com>
2022-02-11 10:50:19 -0300

    criando o projeto

---
+++ src/main/java/cafedamanha/api/CafedamanhaApiApplication.java
@@ -0,0 +1,13 @@
+package cafedamanha.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafedamanhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafedamanhaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafedamanha/api/CafedamanhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cafedamanha.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafedamanhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
